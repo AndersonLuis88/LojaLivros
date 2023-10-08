e9d7628f11a5be73bceeee8cf4828c7ddf11ba54
Anderson Machado <andersonluis1486@gmail.com>
2023-10-08 01:36:16 -0300

    Estrutura Inicial do Projeto

---
+++ LojaLivros/src/building blocks/LojaLivro.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace LojaLivro.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ LojaLivros/src/web/LojaLivro.WebApp.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LojaLivro.WebApp.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
